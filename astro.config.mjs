5783873b21833c1f085d0954c527e21c87ac3235
kyoma102 <steinsgatecqy102@gmail.com>
2025-11-28 18:20:25 +0800

    Update Astro config and deployment workflow to tutorial's recommended setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({ base: '/my-blog/' });
+import { defineConfig } from "astro/config";
+
+const isProd = process.env.NODE_ENV === "production";
+
+// https://astro.build/config
+export default defineConfig({
+  site: isProd ? "https://kyoma102.github.io" : undefined,
+  base: isProd ? "/my-blog" : undefined,
+});
